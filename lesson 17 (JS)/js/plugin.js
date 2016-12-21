de77b5113d448596bd5bf054ad133c2aca6e81a3
Алексей Дмитренко <wow-alexey@mail.ru>
2016-12-21 23:47:43 +0200

    дз по js

---
+++ lesson 17 (JS)/js/plugin.js
@@ -0,0 +1,90 @@
+// var x = 5;
+// var y = 'hello';
+//
+// var z = 10,
+//     v = 'world';
+//
+// var arr = [1, 'hell', 3];
+// var obj = {
+//     user: 'Denis',
+//     age: 27,
+// }
+//
+// var func = function (){
+//
+// }
+//
+// var MAXHEIGHT = 100;
+//
+// x = 10;
+// x = y;
+// var str
+// console.log(str);
+// var str = 'hello world';
+// console.log(str);
+//
+// //console. log(str2);
+// console.log(str2);
+// let str2 = 'sme let';
+// console.log(str2);
+//
+// const  MACX = 1;
+
+// var str1 = 'hello',
+//     str2 = 'world';
+//
+// var str3 = 'hello' + 'world';
+// var z = '';
+//
+// console. log(str3.length);
+// console. log(str3.charAt(1));
+// console. log(str3[2]);
+// console. log(str3.toUpperCase());
+// console. log(str3.indexOf('w'));
+// console. log(str3.substr(2, 5));
+// console. log(str3.substring(2, 6));
+// console. log(str3.slice(2, 6));
+// console. log(str3.charCodeAt(2));
+// console. log(String.fromCharCode(108))
+//
+//
+// var x = '20.23px';
+// console. log(parseInt(x));
+// console. log(parseFloat(x));
+
+
+// var obj = {
+//     name: 'Denis',
+//     age: 27,
+//     info: {
+//         country: 'some',
+//     }
+// }
+// console. log(obj.name);
+// console. log(obj['age']);
+//
+// obj.name = 'Ivan';
+//
+// console. log(obj.name);
+//
+// var property = 'name';
+//
+// obj [property] = 'Ivan2';
+// obj ['work'] = 'eadycode';
+// console. log(obj.info['country']);
+
+// var arr = ['name',1];
+//
+// arr[0] = 'some';
+//
+// console. log(arr);
+
+var x = 10;
+x = x + 10;
+x += 10;
+
+
+
+
+
+
